91b7342f27a54cc012a051f8eec9176a9de637ac
Geertje123 <info@Geertje123.com>
2015-09-08 16:09:30 +0200

    UserScript config setup

---
+++ vBUSdevkit.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         vBUSdevkit
+// @namespace    http://your.vBulletin.site/
+// @version      0.1
+// @description  A dev kit that helps with creating userscripts for vBulletin forum software
+// @author       Geertje123
+// @require      http://code.jquery.com/jquery-2.1.3.min.js
+// ==/UserScript==
+
