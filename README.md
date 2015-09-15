47bc9300583908973fa6395c0d3374a9719e4afe
Geertje123 <info@Geertje123.com>
2015-09-15 10:51:27 +0200

    v1.002 - Got rid of jsdoc. Testing new markdown docs.

--- README.md
+++ README.md
@@ -1,2 +1,218 @@
 # vBUSdevkit
 A dev kit that helps with creating userscripts for vBulletin forum software
+
+## Intro
+I created this for the website http://unknowncheats.me/ to give better support for userscript development. Therefore I haven't tested it on newer versions of vBullitin. If you do encouter problems with newer versions of vBullitin, leave an issue and I'll have a look.
+
+
+## Table of contents
+1. Methods
+  1. Sections
+    1. getAllSections()
+    2. getAllSectionTitles()
+    3. getSectionByIndex(index)
+    4. getSectionByTitle(title)
+  2. Forums
+  3. Threads
+  4. Posts
+
+
+## Methods
+Contains a list of all methods. Note: indexes start at 0. For example; if you want the _first_ thread in a forum, you use `getThreadByIndex(0)`.
+
+### Sections
+These functions should only be used on the main page where there is a view of all sections with its forums.
+
+#### getAllSections()
+Gets all the sectionblocks. Returns the DOM objects in an array.
+
+* param: -
+* returns: Object[]
+
+#### getAllSectionTitles()
+Gets all the titles of the sectionblocks. Returns the titles as strings in an array.
+
+* param: -
+* returns: String[]
+
+#### getSectionByIndex(index)
+Gets the section at the given index. Returns the DOM object of the section.
+
+* param: Number index
+* returns: Object
+
+#### getSectionByTitle(title)
+Gets the section with the given title. Returns the DOM object of the section.
+
+* param: String title
+* returns: Object
+
+
+### Forums
+These methods should only be used on the main page where there is a view of all sections with its forums.
+
+#### getAllForums()
+Gets all the forums and returns them as DOM objects in an array.
+
+* param: -
+* returns: Object[]
+
+#### getAllForumTitles()
+Gets all the forum titles and returns them as strings in an array.
+
+* param: -
+* returns: String[]
+
+#### getAllForumsWithNewPost()
+Gets all the forums with new posts in them and returns them as DOM objects in an array.
+
+* param: -
+* returns: Object[]
+
+#### getAllForumsWithoutNewPost()
+Gets all the forums with no new posts in them and returns them as DOM objects in an array.
+
+* param: -
+* returns: Object[] 
+
+#### getAllForumsFromSectionIndex(index)
+Gets all the forums in the section given by index and returns them as DOM objects in an array.
+
+* param: Number index
+* returns: Object[]
+
+#### getAllForumsFromSectionTitle(title)
+Gets all the forums in the section given by title and returns them as DOM objects in an array.
+
+* param: String title
+* returns: Object[]
+
+#### getForumByIndex(index)
+Gets the forum at the given index and returns it as a DOM object.
+
+* param: Number index
+* returns: Object
+
+#### getForumByTitle(title)
+Gets the forum with the given title and returns it as a DOM object.
+
+* param: String title
+* returns: Object
+
+
+### Threads
+These methods should only be used within a forum where there is a list of threads in it.
+
+#### getAllThreads()
+Gets all the threads in a section. Returns an array with the DOM element of every thread.
+
+* param: -
+* returns: Object[]
+
+#### getAllNonStickyThreads()
+Gets all the non-sticky threads in a section. Returns an array with the DOM element of every thread.
+
+* param: -
+* returns: Object[] 
+
+#### getAllStickyThreads()
+Gets all the sticky threads in a section. Returns an array with the DOM element of every thread.
+
+* param: -
+* returns: Object[]
+
+#### getAllThreadsWithNewPost()
+Gets all the threads with unread posts in them. Returns an array with the DOM element of every thread.
+
+* param: -
+* returns: Object[]
+
+#### getAllThreadsWithoutNewPost()
+Gets all the threads with no unread posts in them. Returns an array with the DOM element of every thread.
+
+* param: -
+* returns: Object[]
+
+#### getAllHotThreads()
+Gets all the threads that are hot. Returns an array with the DOM element of every thread.
+
+* param: -
+* returns: Object[]
+
+#### getAllLockedThreads()
+Gets all the threads that are locked. Returns an array with the DOM element of every thread.
+
+* param: -
+* returns: Object[]
+
+#### getAllThreadsYouPostedIn()
+Gets all the threads that you have posted in. Returns an array with the DOM element of every thread.
+
+* param: -
+* returns: Object[]
+
+#### getThreadByIndex(index)
+Gets the thread at the given index. Thread counting starts at 0. Returns the DOM element of the thread.
+
+* param: Number index
+* returns: Object
+
+#### getThreadByTitle(title)
+Gets the thread with the given title. Returns the DOM object of the thread.
+
+* param: String title
+* returns: Object
+
+#### getThreadTitle(thread)
+Gets the title of the given thread. Takes the thread parameter as a DOM element. Returns the title as string.
+
+* param: Object thread
+* returns: String
+
+#### getThreadAuthor(thread)
+Gets the author of the given thread. Takes the thread parameter as a DOM element. Returns the author as string.
+
+* param: Object thread
+* returns: String
+
+#### getThreadUrl(thread)
+Gets the URL of the given thread. Takes the thread parameter as a DOM element. Returns the URL as string.
+
+* param: Object thread
+* returns: String
+
+#### getThreadLastPostUrl(thread)
+Gets the last post URL of the given thread. Takes the thread parameter as a DOM element. Returns the last post URL as string.
+
+* param: Object thread
+* returns: String
+
+#### getThreadLastPostAuthor(thread)
+Gets the last post author of the given thread. Takes the thread parameter as a DOM element. Returns the last post author as string.
+
+* param: Object thread
+* returns: String
+
+#### getThreadPageAmount(thread)
+Gets the amount of pages of the given thread. Takes the thread parameter as a DOM element. Returns the amount of pages as number.
+
+* param: Object thread
+* returns: Number
+
+#### getThreadReplyAmount(thread)
+Gets the amount of replies of the given thread. Takes the thread parameter as a DOM element. Returns the amount of replies as number.
+
+* param: Object thread
+* returns: Number
+
+#### getThreadViewAmount(thread)
+Gets the amount of views of the given thread. Takes the thread parameter as a DOM element. Returns the amount of views as number.
+
+* param: Object thread
+* returns: Number
+
+
+### Posts
+Soon to be
+
+
--- vBUSdevkit.js
+++ vBUSdevkit.js
@@ -1,23 +1,17 @@
 // ==UserScript==
 // @name         vBUSdevkit
 // @namespace    http://your.vBulletin.site/
-// @version      1.001
+// @version      1.002
 // @description  A dev kit that helps with creating userscripts for vBulletin forum software
 // @author       Geertje123
 // @require      http://code.jquery.com/jquery-2.1.3.min.js
 // ==/UserScript==
 
-/**
- * @namespace VBUS
- * @type {{getAllThreads: Function, getAllNonStickyThreads: Function, getAllStickyThreads: Function, getAllThreadsWithNewPost: Function, getAllThreadsWithoutNewPost: Function, getAllHotThreads: Function, getAllLockedThreads: Function, getAllThreadsYouPostedIn: Function, getThreadByIndex: Function, getThreadTitle: Function, getThreadAuthor: Function, getThreadUrl: Function, getLastPostUrl: Function, getLastPostAuthor: Function, getThreadPageAmount: Function, getThreadReplyAmount: Function, getThreadViewAmount: Function}}
- */
+
 var VBUS = {
-    /**
-     * Gets all the sectionblocks. Returns the DOM objects in an array.
-     * Should only be used on the main page.
-     *
-     * @returns {Object[]}
-     */
+
+
+    /** Sections **/
     getAllSections: function () {
         var sections = [];
 
@@ -27,13 +21,6 @@ var VBUS = {
 
         return sections;
     },
-
-    /**
-     * Gets all the titles of the sectionblocks. Returns the titles as strings in an array.
-     * Should only be used on the main page.
-     *
-     * @returns {String[]}
-     */
     getAllSectionTitles: function () {
         var sections = [];
         var section = {};
@@ -46,14 +33,6 @@ var VBUS = {
 
         return sections;
     },
-
-    /**
-     * Gets the section at the given index. Returns the DOM object of the section.
-     * Should only be used on the main page.
-     *
-     * @param {Number} index
-     * @returns {Object}
-     */
     getSectionByIndex: function (index) {
         var iteration = 0;
         var section = {};
@@ -70,14 +49,6 @@ var VBUS = {
 
         return section;
     },
-
-    /**
-     * Gets the section with the given title. Returns the DOM object of the section.
-     * Should only be used on the main page.
-     *
-     * @param {String} title
-     * @returns {Object}
-     */
     getSectionByTitle: function (title) {
         var match = false;
         var section = {};
@@ -99,16 +70,7 @@ var VBUS = {
     },
 
 
-
-
-
-
-    /**
-     * Gets all the forums and returns them as DOM objects in an array.
-     * Should only be used on the main page.
-     *
-     * @returns {Object[]}
-     */
+    /** Forums **/
     getAllForums: function () {
         var forums = [];
 
@@ -122,14 +84,7 @@ var VBUS = {
 
         return forums;
     },
-
-    /**
-     * Gets all the forum titles and returns them as strings in an array.
-     * Should only be used on the main page.
-     *
-     * @returns {String[]}
-     */
-    getAllForumsTitles: function () {
+    getAllForumTitles: function () {
         var forums = [];
 
         $("tbody[id^='collapseobj_forumbit_']").each(function () {
@@ -142,13 +97,6 @@ var VBUS = {
 
         return forums;
     },
-
-    /**
-     * Gets all the forums with new posts in them and returns them as DOM objects in an array.
-     * Should only be used on the main page.
-     *
-     * @returns {Object[]}
-     */
     getAllForumsWithNewPost: function () {
         var forums = [];
 
@@ -164,13 +112,6 @@ var VBUS = {
 
         return forums;
     },
-
-    /**
-     * Gets all the forums with no new posts in them and returns them as DOM objects in an array.
-     * Should only be used on the main page.
-     *
-     * @returns {Object[]}
-     */
     getAllForumsWithoutNewPost: function () {
         var forums = [];
 
@@ -186,14 +127,6 @@ var VBUS = {
 
         return forums;
     },
-
-    /**
-     * Gets all the forums in the section given by index and returns them as DOM objects in an array.
-     * Should only be used on the main page.
-     *
-     * @param {Number} index
-     * @returns {Object[]}
-     */
     getAllForumsFromSectionIndex: function (index) {
         var iteration = 0;
         var forums = [];
@@ -217,13 +150,6 @@ var VBUS = {
 
         return forums;
     },
-
-    /**
-     * Gets all the forums in the section given by title and returns them as DOM objects in an array.
-     * Should only be used on the main page.
-     *
-     * @returns {Object[]}
-     */
     getAllForumsFromSectionTitle: function (title) {
         var match = false;
         var forums = [];
@@ -250,14 +176,6 @@ var VBUS = {
             return [];
         }
     },
-
-    /**
-     * Gets the forum at the given index and returns it as a DOM object.
-     * Should only be used on the main page.
-     *
-     * @param {Number} index
-     * @returns {Object}
-     */
     getForumByIndex: function (index) {
         var iteration = 0;
         var match = false;
@@ -290,14 +208,6 @@ var VBUS = {
             return {};
         }
     },
-
-    /**
-     * Gets the forum with the given title and returns it as a DOM object.
-     * Should only be used on the main page.
-     *
-     * @param {String} title
-     * @returns {Object}
-     */
     getForumByTitle: function (title) {
         var match = false;
         var forum = {};
@@ -329,16 +239,7 @@ var VBUS = {
     },
 
 
-
-
-
-
-    /**
-     * Gets all the threads in a section. Returns an array with the DOM element of every thread.
-     * Should only be used within a forum.
-     *
-     * @returns {Object[]}
-     */
+    /** Threads **/
     getAllThreads: function () {
         var threads = [];
         var tr = {};
@@ -353,13 +254,6 @@ var VBUS = {
 
         return threads;
     },
-
-    /**
-     * Gets all the non-sticky threads in a section. Returns an array with the DOM element of every thread.
-     * Should only be used within a forum.
-     *
-     * @returns {Object[]}
-     */
     getAllNonStickyThreads: function () {
         var threads = [];
         var pattern = new RegExp("<b>Sticky</b>");
@@ -375,13 +269,6 @@ var VBUS = {
 
         return threads;
     },
-
-    /**
-     * Gets all the sticky threads in a section. Returns an array with the DOM element of every thread.
-     * Should only be used within a forum.
-     *
-     * @returns {Object[]}
-     */
     getAllStickyThreads: function () {
         var threads = [];
         var pattern = new RegExp("<b>Sticky</b>");
@@ -397,13 +284,6 @@ var VBUS = {
 
         return threads;
     },
-
-    /**
-     * Gets all the threads with unread posts in them. Returns an array with the DOM element of every thread.
-     * Should only be used within a forum.
-     *
-     * @returns {Object[]}
-     */
     getAllThreadsWithNewPost: function () {
         var threads = [];
         var tr = {};
@@ -418,13 +298,6 @@ var VBUS = {
 
         return threads;
     },
-
-    /**
-     * Gets all the threads with no unread posts in them. Returns an array with the DOM element of every thread.
-     * Should only be used within a forum.
-     *
-     * @returns {Object[]}
-     */
     getAllThreadsWithoutNewPost: function () {
         var threads = [];
         var tr = {};
@@ -439,13 +312,6 @@ var VBUS = {
 
         return threads;
     },
-
-    /**
-     * Gets all the threads that are hot. Returns an array with the DOM element of every thread.
-     * Should only be used within a forum.
-     *
-     * @returns {Object[]}
-     */
     getAllHotThreads: function () {
         var threads = [];
         var pattern = new RegExp("_hot(_lock)?.gif");
@@ -461,13 +327,6 @@ var VBUS = {
 
         return threads;
     },
-
-    /**
-     * Gets all the threads that are locked. Returns an array with the DOM element of every thread.
-     * Should only be used within a forum.
-     *
-     * @returns {Object[]}
-     */
     getAllLockedThreads: function () {
         var threads = [];
         var pattern = new RegExp("_lock.gif");
@@ -483,13 +342,6 @@ var VBUS = {
 
         return threads;
     },
-
-    /**
-     * Gets all the threads that you have posted in. Returns an array with the DOM element of every thread.
-     * Should only be used within a forum.
-     *
-     * @returns {Object[]}
-     */
     getAllThreadsYouPostedIn: function () {
         var threads = [];
         var pattern = new RegExp("_dot.*\.gif");
@@ -505,14 +357,6 @@ var VBUS = {
 
         return threads;
     },
-
-    /**
-     * Gets the thread at the given index. Thread counting starts at 0. Returns the DOM element of the thread.
-     * Should only be used within a forum.
-     *
-     * @param {Number} index
-     * @returns {Object}
-     */
     getThreadByIndex: function (index) {
         var iteration = 0;
         var match = false;
@@ -537,14 +381,6 @@ var VBUS = {
             return {};
         }
     },
-
-    /**
-     * Gets the thread with the given title. Returns the DOM object of the thread.
-     * Should only be used within a forum.
-     *
-     * @param {String} title
-     * @returns {Object}
-     */
     getThreadByTitle: function (title) {
         var match = false;
         var tr = {};
@@ -566,69 +402,21 @@ var VBUS = {
             return {};
         }
     },
-
-    /**
-     * Gets the title of the given thread. Takes the thread parameter as a DOM element. Returns the title as string.
-     * Should only be used within a forum.
-     *
-     * @param {Object} thread
-     * @returns {String}
-     */
     getThreadTitle: function (thread) {
         return $(thread).find("a[id^='thread_title_']").text();
     },
-
-    /**
-     * Gets the author of the given thread. Takes the thread parameter as a DOM element. Returns the author as string.
-     * Should only be used within a forum.
-     *
-     * @param {Object} thread
-     * @returns {String}
-     */
     getThreadAuthor: function (thread) {
         return $(thread).find("td[id^='td_threadtitle_']").find("div[class='smallfont']").find("span").text();
     },
-
-    /**
-     * Gets the URL of the given thread. Takes the thread parameter as a DOM element. Returns the URL as string.
-     * Should only be used within a forum.
-     *
-     * @param {Object} thread
-     * @returns {String}
-     */
     getThreadUrl: function (thread) {
         return $(thread).find("a[id^='thread_title_']").attr("href");
     },
-
-    /**
-     * Gets the last post URL of the given thread. Takes the thread parameter as a DOM element. Returns the last post URL as string.
-     * Should only be used within a forum.
-     *
-     * @param {Object} thread
-     * @returns {String}
-     */
-    getLastPostUrl: function (thread) {
+    getThreadLastPostUrl: function (thread) {
         return $(thread).find("img[alt='Go to last post']").parent().attr("href");
     },
-
-    /**
-     * Gets the last post author of the given thread. Takes the thread parameter as a DOM element. Returns the last post author as string.
-     * Should only be used within a forum.
-     *
-     * @param {Object} thread
-     * @returns {String}
-     */
-    getLastPostAuthor: function (thread) {
+    getThreadLastPostAuthor: function (thread) {
         return $(thread).find("img[alt='Go to last post']").parent().parent().find("a[href*='forum/member']").text();
     },
-
-    /**
-     * Gets the amount of pages of the given thread. Takes the thread parameter as a DOM element. Returns the amount of pages as number.
-     * Should only be used within a forum.
-     *
-     * @param {Object} thread
-     * @returns {Number}
-     */
     getThreadPageAmount: function (thread) {
         var number = $(thread).find("img[title='Multi-page thread']").parent().find("a").last().text();
 
@@ -638,25 +426,9 @@ var VBUS = {
             return parseInt(number)
         }
     },
-
-    /**
-     * Gets the amount of replies of the given thread. Takes the thread parameter as a DOM element. Returns the amount of replies as number.
-     * Should only be used within a forum.
-     *
-     * @param {Object} thread
-     * @returns {Number}
-     */
     getThreadReplyAmount: function (thread) {
         return parseInt($(thread).find("a[href*='forum/misc.php?do=whoposted&t=']").text());
     },
-
-    /**
-     * Gets the amount of views of the given thread. Takes the thread parameter as a DOM element. Returns the amount of views as number.
-     * Should only be used within a forum.
-     *
-     * @param {Object} thread
-     * @returns {Number}
-     */
     getThreadViewAmount: function (thread) {
         return parseInt($(thread).find("a[href*='forum/misc.php?do=whoposted&t=']").parent().next().text());
     }
